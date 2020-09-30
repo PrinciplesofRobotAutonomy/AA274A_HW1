b5585697bffa001b4f6911a40cdf0564b6cb3fda
Joe Lorenzetti <jlorenze@stanford.edu>
2020-09-30 13:32:06 -0700

    switch from scikits bvp solver to scipy bvp solver

--- P4_optimal_control.py
+++ P4_optimal_control.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-import scikits.bvp_solver
+from scipy.integrate import solve_bvp as tpbvp
 import matplotlib.pyplot as plt
 from utils import *
 
@@ -10,11 +10,12 @@ def ode_fun(tau, z):
     """
     This function computes the dz given tau and z. It is used in the bvp solver.
     Inputs:
-        tau: the independent variable. This must be the first argument.
-        z: the state vector. The first three states are [x, y, th, ...]
+        tau: (np.array shape [N,]) the independent variables. This must be the first argument.
+        z: (np.array shape [n_z,N]) the state vectors. The first three states are z[:,i] = [x, y, th, ...]
     Output:
-        dz: the state derivative vector. Returns a numpy array.
+        dz: (np.array shape [n_z,N]) the state derivative vectors. Returns a numpy array.
     """
+    (n_z, N) = np.shape(z)
     ########## Code starts here ##########
 
     ########## Code ends here ##########
@@ -28,8 +29,7 @@ def bc_fun(za, zb):
         za: the state vector at the initial time
         zb: the state vector at the final time
     Output:
-        bca: tuple of boundary conditions at initial time
-        bcb: tuple of boundary conditions at final time
+        bc: tuple of boundary conditions
     """
     # final goal pose
     xf = 5
@@ -42,23 +42,23 @@ def bc_fun(za, zb):
     ########## Code starts here ##########
 
     ########## Code ends here ##########
-    return (bca, bcb)
+    return np.concatenate((bca, bcb))
 
 def solve_bvp(problem_inputs, initial_guess):
     """
     This function solves the bvp_problem.
     Inputs:
         problem_inputs: a dictionary of the arguments needs to define the problem
-                        num_ODE, num_parameters, num_left_boundary_conditions,
-                        boundary_points, function, boundary_conditions
+                        fun, bc, x
         initial_guess: initial guess of the solution
     Output:
         z: a numpy array of the solution. It is of size [time, state_dim]
 
-    Read this documentation -- https://pythonhosted.org/scikits.bvp_solver/tutorial.html
+    Read this documentation -- https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_bvp.html
     """
-    problem = scikits.bvp_solver.ProblemDefinition(**problem_inputs)
-    soln = scikits.bvp_solver.solve(problem, solution_guess=initial_guess)
+    result = tpbvp(y=initial_guess, verbose=2, **problem_inputs)
+    soln = result['sol']
+    print(result['message'])
 
     # Test if time is reversed in bvp_solver solution
     flip, tf = check_flip(soln(0))
@@ -99,12 +99,9 @@ def main():
     ########## Code ends here ##########
 
     problem_inputs = {
-                      'num_ODE' : num_ODE,
-                      'num_parameters' : num_parameters,
-                      'num_left_boundary_conditions' : num_left_boundary_conditions,
-                      'boundary_points' : boundary_points,
-                      'function' : function,
-                      'boundary_conditions' : boundary_conditions
+                      'fun' : function,
+                      'bc'  : boundary_conditions,
+                      'x'   : tau,
                      }
 
     z = solve_bvp(problem_inputs, initial_guess)
